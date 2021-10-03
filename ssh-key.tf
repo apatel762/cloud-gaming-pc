475cc716957ee04dd43632b536013eebe7090081
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2021-10-03 16:09:54 +0100

    add extra comments to ssh-key.tf

--- ssh-key.tf
+++ ssh-key.tf
@@ -1,3 +1,17 @@
+# Used a mixture of the below references to get this set up:
+#
+#   https://ifritltd.com/2017/12/06/provisioning-ec2-key-pairs-with-terraform/
+#   https://stackoverflow.com/questions/49743220/how-do-i-create-an-ssh-key-in-terraform
+#   https://www.phillipsj.net/posts/generating-ssh-keys-with-terraform/
+#
+# Using the `local_file` resource can create noise in the terraform apply
+# command if multiple people are deploying the same thing, as it must create
+# the local file for each of you (and will report the file as missing for
+# each new person who deploys the infra in this repo)
+#
+# This isn't a problem for my use case though, as I'm the only person using
+# this repo.
+
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = "4096"
