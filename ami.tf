ee35d6f43f50ce01becfaa26106a61e8672d3b0a
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2021-10-04 19:17:12 +0100

    bump Ubuntu AMI version to 21.04

--- ami.tf
+++ ami.tf
@@ -3,12 +3,22 @@
 data "aws_ami" "ubuntu_ami" {
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-groovy-20.10-arm64-server-*"]
+    values = ["ubuntu*-21.04-arm64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "ena-support"
+    values = ["true"]
   }
 
   most_recent = true
